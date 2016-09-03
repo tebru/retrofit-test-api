119554daaa21c0a25676b729762d1ab9c01f51cc
Nate Brunette <n@tebru.net>
2016-09-03 12:15:45 -0500

    Initial commit of retrofit api
    Signed-off-by: Nate Brunette <n@tebru.net>

---
+++ README.md
@@ -0,0 +1,4 @@
+Retrofit Test Api
+=================
+
+This is a Symfony project created to make functional testing for tebru/retrofit-php easier.
---
+++ src/AppBundle/AppBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Tebru\RetrofitApi\AppBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AppBundle extends Bundle
+{
+}
